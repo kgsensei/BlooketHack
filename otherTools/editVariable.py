2da25c0768239447ac3217917f88d62e4b86455f
Jeremy <60828719+kgsensei@users.noreply.github.com>
2021-05-05 11:26:11 -0400

    Update editVariable.py

--- otherTools/editVariable.py
+++ otherTools/editVariable.py
@@ -7,11 +7,10 @@ accName=str(input("Account Name: "))
 namself=str(input("Nickname: "))
 aic=str(input("Animal [See Options]: "))
 while True:
-     cPlace=str(input("Place: "))
      amount=str(input("Amount: "))
      #----------------------------------------------------------
      #           Request is Unauthorized - Progress
      #----------------------------------------------------------
-     r=requests.put(f'https://api.blooket.com/api/users/goldstats',headers={'Referer':'https://www.blooket.com/','name':accName,'place':cPlace,'gold':amount,'nameUsed':namself,'blookUsed':aic})
+     r=requests.put(f'https://api.blooket.com/api/users/goldstats',headers={'Referer':'https://www.blooket.com/','name':accName,'gold':amount,'nameUsed':namself,'blookUsed':aic})
      print(r.text)
-     time.sleep(5)
+     time.sleep(1)
