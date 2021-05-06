6ff99cb594712a53f0fceb11f8027770bd633b91
Jeremy <60828719+kgsensei@users.noreply.github.com>
2021-05-06 08:27:29 -0400

    Update floodGame.py

--- otherTools/floodGame.py
+++ otherTools/floodGame.py
@@ -3,5 +3,5 @@ import requests, json
 gamePin=str(input("Game pin: "))
 botsInt=int(input("Bots: "))
 for i in range(0,botsInt):
-  requests.put("https://api.blooket.com/api/firebase/join",data={"id":gamePin,"name":"blooketbad"},headers={"Referer":"https://www.blooket.com/"})
+  requests.put("https://api.blooket.com/api/firebase/join",data={"id":gamePin,"name":"Bot"+str(i)},headers={"Referer":"https://www.blooket.com/"})
   
