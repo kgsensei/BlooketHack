b6e699b2ab8447d959e325c17cd22594deea9302
Jeremy <60828719+kgsensei@users.noreply.github.com>
2021-05-06 08:27:52 -0400

    Patch Flood Slowdown

--- otherTools/floodGame.py
+++ otherTools/floodGame.py
@@ -4,4 +4,4 @@ gamePin=str(input("Game pin: "))
 botsInt=int(input("Bots: "))
 for i in range(0,botsInt):
   requests.put("https://api.blooket.com/api/firebase/join",data={"id":gamePin,"name":"Bot"+str(i)},headers={"Referer":"https://www.blooket.com/"})
-  
+ 
