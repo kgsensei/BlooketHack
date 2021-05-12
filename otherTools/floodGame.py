23b113d835514669168f5293f109ceb494c9fa2d
Jeremy <60828719+kgsensei@users.noreply.github.com>
2021-05-12 14:39:11 -0400

    Update floodGame.py

--- otherTools/floodGame.py
+++ otherTools/floodGame.py
@@ -1,4 +1,9 @@
-import requests, json
+try:
+  import requests,json
+except Exception:
+  import os
+  os.system("pip install requests")
+  import requests,json
 
 gamePin=str(input("Game pin: "))
 botsInt=int(input("Bots: "))
